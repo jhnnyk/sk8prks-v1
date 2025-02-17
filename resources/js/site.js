12b3fdb61dc801ddb4b28a320cad1d0131e55874
John Knotts <jhnnyk@gmail.com>
2025-02-16 17:04:01 -0700

    functional but ugly hamburger menu

--- resources/js/site.js
+++ resources/js/site.js
@@ -1,1 +1,14 @@
-// This is all you.
+// hamburger menu navigation
+const hamMenu = document.querySelector(".ham-menu");
+
+const offScreenMenu = document.querySelector(".off-screen-menu");
+
+hamMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
+
+offScreenMenu.addEventListener("click", () => {
+  hamMenu.classList.toggle("active");
+  offScreenMenu.classList.toggle("active");
+});
